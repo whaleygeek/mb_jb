f05a120aa8e8ed3fd8029b521abdcd8d4caca4b9
David Whale <david@thinkingbinaries.com>
2015-12-14 20:52:15 +0000

    Working on findpulse.py Looks like it is nontransparrent in the receive. netcat both ends sends transparently. netcat in sender, findpulse.py receiver, chomps '13' chars. Done this by comparing received data against binary source file. This causes the low/high bytes to swap and thus then the pulse detector no longer works.
    Need look at socket options on a socket to see if it has a default
    newline mode.
    
    Also, seems to receive a load of bytes, then a 1 byte. This might
    be related to an early terminate with a newline character on receive.

---
+++ findpulse.py
@@ -0,0 +1,81 @@
+# findpulse.py  14/12/2015  D.J.Whale
+#
+# Listen on a nominated server socket.
+# when a connection comes in, open it.
+# read data 2 bytes at a time as a U16 (endianness??)
+# low time is time below a threshold
+# high time is time above a threshold
+# show time of each low/high as it is seen.
+# close down safely when connection lost, and listen again.
+
+import time
+import network
+
+SERVER_PORT = 3001
+THRESHOLD = 32768/4
+
+b1 = None
+phase = 0
+count = 0
+
+binfile = open("bytestream.bin", "rb")
+
+def incoming(msg):
+    global b1, phase, count, prev
+
+    print("msglen:%d" % len(msg))
+    #if len(msg) == 1:
+    #    print("single byte: %d prev was %d" % (ord(msg[0]), prev))
+
+    for c in msg:
+        b = ord(c)
+        fb = ord(binfile.read(1))
+        print(fb, b)
+        if b != fb:
+            print("diff: file:%d  net:%d" % (fb, b))
+
+        if b1 == None:
+            b1 = b
+        else:
+            v = b1 + 256 * b # low/high (little-endian)
+            # signed integers (note long lead in at +max in sample data)
+            if v > 32767:
+                v = -(65536 - v) # two's compliment negative data value
+            #print(v,b1,b)
+            b1 = None
+            if phase == 0:
+                if v < THRESHOLD:
+                    count += 1
+                else:
+                    #print("low for: %d, but now %d" % (count, v))
+                    phase = 1
+                    count = 1
+
+            elif phase == 1:
+                if v >= THRESHOLD:
+                    count += 1
+                else:
+                    #print("high for: %d, but now %d" % (count, v))
+                    phase = 0
+                    count = 1
+
+
+def run_listener():
+    try:
+        while True:
+            print("waiting for connection...")
+            network.wait(whenHearCall=incoming, port=SERVER_PORT)
+            print("connected!")
+
+            while network.isConnected():
+                time.sleep(1)
+
+            print("connection lost!")
+    finally:
+        network.hangUp()
+
+
+if __name__ == "__main__":
+    run_listener()
+
+# END
